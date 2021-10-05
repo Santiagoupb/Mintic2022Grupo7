594ed19eebfd8af07a9ffd2c146390002e09076d
Santiagoupb <91201156+Santiagoupb@users.noreply.github.com>
2021-10-05 15:13:57 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-let aspectBtn = document.getElementById("aspecto");
-let mainElement = document.getElementById("container");
-
-aspectBtn.addEventListener("click", function(){
-	mainElement.classList.toggle("dark");
-});
