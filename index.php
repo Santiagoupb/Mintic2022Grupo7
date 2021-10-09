8d2e71b1cdabd7ff73421e3d00d700f615614fa2
Jeimy Stéffany Aristizábal Pérez <91352607+Steffany98@users.noreply.github.com>
2021-10-08 21:24:53 -0500

    Create index.php
    Avance de front del index

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+<head>
+<style>
+
+
+li {
+  float: right;
+  border-right:3px solid #bbb;
+}
+
+li:last-child {
+  border-right: none;
+}
+
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 18px 16px;
+  text-decoration: none;
+  font: 100% sans-serif;
+  font-weight: bold;
+}
+
+body { background-color: #FCD98D; }
+</style>
+</head>
+</body>
+    <title>Helpdesk Pro</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-muted" id="barra" >
+  <div class="container-fluid">
+  <img src="Img/logo.png" id="loguito" class="rounded float-start">
+  <style>#loguito{
+    min-width: 50px;
+    max-width: 50px;
+  } 
+  </style>
+
+
+  
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  <div class="d-flex flex-row-reverse bd-highlight">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+
+          <li class="nav-item" >
+            <a class="nav-link" href="#equipo" id="color">Nuestro equipo</a>
+          </li>
+
+
+          <li class="nav-item" >
+            <a class="nav-link" href="/HELPDESK/Views/Estadopedido.php" id="color">Consultar pedido</a>
+          </li>
+
+
+          <li class="nav-item" >
+            <a class="nav-link" href="/HELPDESK/Views/login.php" id="color">Login/Registro</a>
+          </li>
+
+
+          <li class="nav-item" >
+            <a class="nav-link active" aria-current="page" href="#home" id="color">Home</a>
+          </li>
+
+      </ul>
+    </div>
+  </div>
+
+</nav>
+
+
+
+
+
+
+<div class="container">
+<div class="d-flex justify-content-center ">
+<h1>hola</h1>    
+
+</div>
+
+    <center> 
+        <div class="container"> 
+  
+            <h1 style=
+       "text-align:center;color:green;"> 
+            GeeksforGeeks 
+        </h1> 
+            <h3> 
+            To make vertical scrollable
+            in a bootstrap row? 
+        </h3> 
+            <div class="container vertical-scrollable"> 
+                <div class="row text-center"> 
+                    <div class="col-sm-8" id="vertical-scrollable">
+                      1</div> 
+                    <div class="col-sm-8" id="vertical-scrollable">
+                      2</div> 
+                    <div class="col-sm-8" id="vertical-scrollable">
+                      3</div> 
+                    <div class="col-sm-8" id="vertical-scrollable">
+                      4</div> 
+                    <div class="col-sm-8" id="vertical-scrollable">
+                      5</div> 
+                    <div class="col-sm-8" id="vertical-scrollable">
+                      6</div> 
+                    <div class="col-sm-8" id="vertical-scrollable">
+                      7</div> 
+                </div> 
+            </div> 
+        </div> 
+    </center>
+</div>
+
+
+
+
+
+
+<div class="container">
+  <footer class="py-3 my-4" id="piepag">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-primary" >Inicio</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-primary" >Sobre nosotros</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-primary" >Precios</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2021 Grupo 7 NRC</p>
+  </footer>
+</div>
+
+</body>
+</html>
